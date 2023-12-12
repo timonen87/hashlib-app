f112f694bf74ac10606c2e288a9315df4f6fbd5d
timonen87 <timmonen.as@gmail.com>
2023-12-12 23:17:42 +0300

    add tests playwright

--- tests/products-list.spec.ts
+++ tests/products-list.spec.ts
@@ -1,20 +1,14 @@
 import { test, expect } from "@playwright/test";
 
-test("create delete product list", async ({ page }) => {
-  await page.locator("body").click();
-  await page.goto("http://app.hashlib.ru");
+test("test create delete product list", async ({ page }) => {
+  await page.goto("/");
   await page.getByPlaceholder("название").click();
-  await page.getByPlaceholder("название").fill("Test course");
+  await page.getByPlaceholder("название").fill("name");
   await page.getByPlaceholder("описание").click();
-  await page.getByPlaceholder("описание").fill("Test description");
+  await page.getByPlaceholder("описание").fill("description");
   await page.getByRole("button", { name: "Добавить" }).click();
-  await expect(
-    page.getByText("Test courseTest descriptionУдалить"),
-  ).toBeVisible();
-
+  await expect(page.getByText("namedescription")).toBeVisible();
+  await expect(page.getByRole("button", { name: "Удалить" })).toBeVisible();
+  await expect(page.getByText("namedescription")).toBeVisible();
   await page.getByRole("button", { name: "Удалить" }).click();
-
-  await expect(
-    page.getByText("Test courseTest descriptionУдалить"),
-  ).not.toBeVisible();
 });
