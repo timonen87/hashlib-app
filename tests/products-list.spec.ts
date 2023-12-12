24c65f86f6e881c0f87cc117d7fad9e044df4d77
timonen87 <timmonen.as@gmail.com>
2023-12-12 23:23:36 +0300

    add tests playwright 1

--- tests/products-list.spec.ts
+++ tests/products-list.spec.ts
@@ -7,7 +7,7 @@ test("test create delete product list", async ({ page }) => {
   await page.getByPlaceholder("описание").click();
   await page.getByPlaceholder("описание").fill("description");
   await page.getByRole("button", { name: "Добавить" }).click();
-  await expect(page.getByText("namedescription")).toBeVisible();
+  await expect(page.getByText("namedescription")).not.toBeVisible();
   await expect(page.getByRole("button", { name: "Удалить" })).toBeVisible();
   await expect(page.getByText("namedescription")).toBeVisible();
   await page.getByRole("button", { name: "Удалить" }).click();
