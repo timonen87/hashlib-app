223a9bba750f2a12efa8c9784b2d735d49016471
timonen87 <timmonen.as@gmail.com>
2023-12-15 01:02:14 +0300

    add app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import { Button } from "@/shared/ui/button";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col  p-12">
+    <main className="flex min-h-screen flex-col  p-8">
+      <h1 className="text-3xl mb-2">App.hashlib.ru</h1>
+
       <CreateProductForm
         revalidatePagePath="/"
-        className="max-w-[300px] mb-20"
+        className="max-w-[300px] mb-10"
       />
 
       <ProductsList revalidatePagePath="/" />
