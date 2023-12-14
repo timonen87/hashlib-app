b0e34ea1d8556e3068237ab0039fcd09284db187
timonen87 <timmonen.as@gmail.com>
2023-12-14 23:52:29 +0300

    fix main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ import { Button } from "@/shared/ui/button";
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col  p-8">
-      <h1 className="text-3xl mb-2">Добавить продукт</h1>
-
       <CreateProductForm
         revalidatePagePath="/"
         className="max-w-[300px] mb-10"
