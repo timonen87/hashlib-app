b3b2710002ae96892af8112eff765d3d17b203a9
timonen87 <timmonen.as@gmail.com>
2023-12-15 00:37:50 +0300

    fix style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { Button } from "@/shared/ui/button";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col  p-8">
+    <main className="flex min-h-screen flex-col  p-12">
       <CreateProductForm
         revalidatePagePath="/"
-        className="max-w-[300px] mb-10"
+        className="max-w-[300px] mb-20"
       />
 
       <ProductsList revalidatePagePath="/" />
