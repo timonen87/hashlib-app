ec993f94ab4df4ec883e1d20f41653bdf6d753fe
timonen87 <timmonen.as@gmail.com>
2024-01-12 16:15:22 +0300

    add h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/shared/ui/button";
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col  p-8">
-      <h1 className="text-3xl mb-2">App</h1>
+      <h1 className="text-3xl mb-2">New App</h1>
 
       <CreateProductForm
         revalidatePagePath="/"
