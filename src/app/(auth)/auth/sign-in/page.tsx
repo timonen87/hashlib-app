2fc7603fd0ae55ca9f4d5e3784da58009730c1e1
timonen87 <timmonen.as@gmail.com>
2024-02-01 11:50:57 +0300

    fix login

--- src/app/(auth)/auth/sign-in/page.tsx
+++ src/app/(auth)/auth/sign-in/page.tsx
@@ -8,9 +8,7 @@ export default function AuthenticationPage() {
       <div className="container relative  flex-col items-center justify-center self-center pt-24">
         <Card className="max-w-[350px] mx-auto">
           <CardHeader className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Войти в аккаунт
-            </h1>
+            <h1 className="text-2xl font-semibold tracking-tight">Войти</h1>
           </CardHeader>
           <CardContent className="grid gap-4">
             {/* <SignInForm /> */}
