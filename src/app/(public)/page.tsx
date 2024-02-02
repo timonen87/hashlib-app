a77eccbdcae9314429e9622b3cfd42eb4fac1694
timonen87 <timmonen.as@gmail.com>
2024-02-02 16:42:59 +0300

    fix h1

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -4,7 +4,7 @@ import { PostsList } from "@/features/posts-list/pub/posts-list";
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col  p-8">
-      <h1 className="text-3xl mb-2">Посты</h1>
+      <h1 className="text-3xl mb-2">Все посты</h1>
       <CreatePostForm revalidatePagePath="/" className="max-w-[300px] mb-10" />
       <PostsList revalidatePagePath="/" />
     </main>
