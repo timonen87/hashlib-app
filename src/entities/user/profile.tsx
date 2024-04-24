e085e078919252ad8bff6485aafebee0ba8cca47
timonen87 <71833451+timonen87@users.noreply.github.com>
2024-04-24 13:06:12 +0300

    add progile features

--- src/entities/user/profile.tsx
+++ src/entities/user/profile.tsx
@@ -1,2 +1,5 @@
 export { getProfileDisplayName } from "./_vm/get-profile-display-name";
 export { ProfileAvatar } from "./_ui/profile-avatar";
+export { useInvalidateProfile, getProfileQuery } from "./_queries";
+export type { Profile } from "./_domain/types";
+export { profileSchema } from "./_domain/schema";
--- src/shared/config/private.ts
+++ src/shared/config/private.ts
@@ -13,6 +13,12 @@ const privateConfigSchema = z.object({
   EMAIL_SERVER_HOST: z.string(),
   EMAIL_SERVER_PORT: z.string(),
   EMAIL_FROM: z.string(),
+
+  S3_ACCESS_KEY_ID: z.string(),
+  S3_SECRET_ACCESS_KEY: z.string(),
+  S3_IMAGES_BUCKET: z.string(),
+  S3_ENDPOINT: z.string(),
+  S3_REGION: z.string(),
 });
 
 export const privateConfig = privateConfigSchema.parse(process.env);
